04b4030248bbe33059e5561bca7e51136f34d812
KimDongHyub <50415338+KimDongHyub@users.noreply.github.com>
2022-03-05 04:04:42 +0900

    Update inverted_index.cpp

--- Projects/project0/inverted_index.cpp
+++ Projects/project0/inverted_index.cpp
@@ -3,109 +3,88 @@
 #include <string>
 #include <map>
 #include <set>
+#include <sstream>
 
 using namespace std;
 
-
-void printfile(string filename){
-    ifstream file(filename);
-    string line;
-    if(file.is_open()){
-        while(getline(file, line)){
-            cout<<line<<endl;
-        }
-        file.close();
-
-    }
-    else{
-        cout<<"Error";
-        
-    }
-}
-
-
-void insertValue(map<string, set<int> >& myMap,
-                 string const& key,
-                 int const& value)
+void insertValue(map<string, set<int> > &myMap,
+                 string const &key,
+                 int const &value)
 {
-   // Check whether there is already a set given the key.
-   // If so, insert to the existing set.
-   // Otherwise, create a set and add it to the map.
-   map<string, set<int> >::iterator found = myMap.find(key);
-   if ( found != myMap.end() )
-   {
-      
-      found->second.insert(value);
-   }
-   else
-   {
-      
-      set<int> temp;
-      temp.insert(value);
-      myMap.insert(make_pair(key, temp));
-   }
+    // Check whether there is already a set given the key.
+    // If so, insert to the existing set.
+    // Otherwise, create a set and add it to the map.
+    map<string, set<int> >::iterator found = myMap.find(key);
+    if (found != myMap.end())
+    {
+
+        found->second.insert(value);
+    }
+    else
+    {
+
+        set<int> temp;
+        temp.insert(value);
+        myMap.insert(make_pair(key, temp));
+    }
 }
+int main(int argc, char* argv[])   //argv 는 터미널 실행 시 실행 커맨드 뒤에 붙는 문자열을 받아준다. ex) ./inverter inputs.txt
+{
+    string line, filename, word, microword;
 
+    map<string, set<int> > invertedIndex;
+    map<string, set<int> >::const_iterator pos; // iterator for loop of map
+    set<int>::iterator set_pos;                 // iterator for loop of index of file
+    int it = 0;
+    int i;
+    filename=argv[1];  // 받아준 문자열을 넘겨준다
+    //cin >> filename;
 
-int main(){
-    string line, filename;
-    
-    map<string,set<int> > invertedIndex;
-    map<string,set<int> >::const_iterator pos;
-    set<int>::iterator set_pos;
-    int it=0;
-    
-
-    cout<<"input filename: "<<endl;
-    cin>>filename;
-    printfile(filename);
-    
     /////////////show each file's context in match index with file////////////
     ifstream file(filename);
-    if(file.is_open()){
-        while(getline(file, line)){
-            cout<<line<<endl;
-            printfile(line);
-            
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
+
             ifstream words(line);
-            string word;
-            if(words.is_open()){
-                while(words >> word){
-                    if(word[word.length()-1]=='.'){
-                        word.erase(word.end()-1);
+            if (words.is_open())
+            {
+                while (words >> word)
+                {
+                    for (i = 0; word[i] != '\0'; i++)
+                    {
+                        if ((word[i] < 'A' || word[i] > 'Z') && (word[i] < 'a' || word[i] > 'z'))
+                        {
+                            word.replace(i, 1, " "); // replace everything except alphabet with space. so string stream can separate it with space
+                        }
+                    }
+
+                    stringstream ss(word);   //without this section word like t2is cannot be separated 
+                    ss.str(word);
+                    while (ss >> microword)
+                    {
+                        insertValue(invertedIndex, microword, it);
+                    }
                 }
-                    
-                    insertValue(invertedIndex,word,it);
-
-        }
-            words.close();
-
-    }
-    else{
-        cout<<"Error";
-        
-    }
-
-
-            
-            
+                words.close();
+            }
+            else
+            {
+                cout << "";
+            }
             it++;
-
-    }
-        
+        }
         file.close();
     }
-    
-    for (pos=invertedIndex.begin(); pos!=invertedIndex.end(); pos++){
-        cout<<pos->first<<": ";
-        for(set_pos=pos->second.begin();set_pos!=pos->second.end();set_pos++){
-            cout<<*set_pos<<" ";
-        
+    for (pos = invertedIndex.begin(); pos != invertedIndex.end(); pos++)
+    {
+        cout << pos->first << ": ";
+        for (set_pos = pos->second.begin(); set_pos != pos->second.end(); set_pos++)
+        {
+            cout << *set_pos << " ";
         }
-        cout<<endl;
-
+        cout << endl;
     }
-    ///////////////////////////////////////////////
-    
     return 0;
 }
